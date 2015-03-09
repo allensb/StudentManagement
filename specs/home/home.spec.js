ffe24c67c8b5b8cd3929685a926fc605724af6b0
Allen Buckley <abuckley@fusionalliance.com>
2015-03-09 11:22:08 -0400

    Updated home.spec

--- specs/home/home.spec.js
+++ specs/home/home.spec.js
@@ -5,33 +5,34 @@ describe('home spec', function() {
     var service,
         scope,
         controller,
-        $httpBackend;
+        httpBackend;
 
-    beforeEach(inject(function(studentApi, $rootScope, $controller, _$httpBackend_){
+    beforeEach(inject(function(studentApi, $rootScope, $controller, $httpBackend){
         service = studentApi;
-        $httpBackend = _$httpBackend_;
+        httpBackend = $httpBackend;
         scope = $rootScope.$new();
         controller = $controller;
     }));
 
     afterEach(function() {
-        $httpBackend.verifyNoOutstandingExpectation();
-        $httpBackend.verifyNoOutstandingRequest();
+        httpBackend.verifyNoOutstandingExpectation();
+        httpBackend.verifyNoOutstandingRequest();
     });
 
     it("should make an ajax call to api/students", function () {
 
-        $httpBackend.expectGET('app/views/home/home.html').respond(200);
-        $httpBackend.whenGET("http://localhost:7000/api/student/1").respond({
-            id: 7,
+        httpBackend.expectGET('app/views/home/home.html').respond(200);
+        httpBackend.whenGET("http://localhost:7000/api/student/1").respond(200, {
+            id: 7, // Data returned, but not set in scope
             first: 'Chris',
             last: 'Johnson'
         });
 
         controller("UpdateCtrl", {$scope: scope, studentApi: service, $routeParams: {id: 1}});
 
-        $httpBackend.flush();
+        httpBackend.flush();
 
         expect(scope.first).toBe('Chris');
+        expect(scope.last).toBe('Johnson');
     });
 });
